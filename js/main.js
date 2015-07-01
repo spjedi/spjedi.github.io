5976e73cd688201fcbfd732d4cc87ecb536ce7e5
Anne Tsugunyan <anne.tsugunyan@gmail.com>
2015-07-01 18:27:00 +0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){
+	$('.clients-slider').slick({
+		infinite: true,
+		dots: false,
+		speed: 100,
+		autoplay: true,
+		arrows: false,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		responsive: [
+		{
+			breakpoint: 1024,
+			settings: {
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			infinite: true,
+			dots: false
+			}
+		},
+		{
+			breakpoint: 850,
+			settings: {
+			slidesToShow: 3,
+			slidesToScroll: 1
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+			slidesToShow: 2,
+			slidesToScroll: 1
+			}
+		},
+		{
+			breakpoint: 580,
+			settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1
+			}
+		}
+		]
+		});
+	$('.testimonials').slick({
+		infinite: true,
+		dots: false,
+		speed: 400,
+		autoplay: true,
+		arrows: false,
+		verticalSwiping: true,
+		vertical: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	});
+	// resize header on scroll
+	$(window).on('scroll', headerResizeHandler);
+
+	function headerResizeHandler() {
+		var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+			shrinkOn = 0,
+			$header = $('.navbar');
+
+		if (distanceY > shrinkOn) {
+			$header.addClass("smaller");
+		} else {
+			$header.removeClass("smaller");
+		}
+	}
+
+	headerResizeHandler();
+
+	// Google map
+	 function initialize() {
+        var mapCanvas = document.getElementById('map-canvas');
+        var mapOptions = {
+          center: new google.maps.LatLng(33.3111327, -111.9770588,665),
+          zoom: 20,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions)
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+});
