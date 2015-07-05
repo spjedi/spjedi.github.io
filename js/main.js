af03af2ae507d16d45f2c9d80b1e9223b4fa2ecf
Anne Tsugunyan <anne.tsugunyan@gmail.com>
2015-07-06 02:23:55 +0400

    Project page

--- js/main.js
+++ js/main.js
@@ -72,8 +72,23 @@ $(document).ready(function(){
 			$header.removeClass("smaller");
 		}
 	}
-
 	headerResizeHandler();
+
+	// Fancy box
+	$("a#single_image").fancybox();
+	
+	$("a#inline").fancybox({
+		'hideOnContentClick': true
+	});
+	
+	$("a.group").fancybox({
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	600, 
+		'speedOut'		:	200, 
+		'overlayShow'	:	false
+	});
+
 
 	// Google map
 	 function initialize() {
