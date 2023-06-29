8128cc6b1d52b955c2eb302b696090c6ab191d5d
ares9515 <godknight122@gmail.com>
2023-06-30 03:19:12 +0900

    1

--- website/assets/arowb/arowB_edgeActions.js
+++
@@ -1,8 +0,0 @@
-
-(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
-//Edge symbol: 'stage'
-(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1250,function(sym,e){sym.play("a");});
-//Edge binding end
-})("stage");
-//Edge symbol end:'stage'
-})(jQuery,AdobeEdge,"EDGE-11914410");
